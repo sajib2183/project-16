94d9bf214deed7293f04bc64e3cb9217bab1680a
sajib2183 <74869039+sajib2183@users.noreply.github.com>
2020-12-13 14:28:48 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayspeed:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
